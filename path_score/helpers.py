ad59d250d2231e497cfa87be019ab78937475007
Suraj Rathi <suraj.rathi00@gmail.com>
2021-01-08 13:38:00 +0530

    Add collision path and environment

--- path_score/helpers.py
+++ path_score/helpers.py
@@ -1,6 +1,5 @@
-import dataclasses
 from dataclasses import dataclass
-from typing import TypeVar
+from typing import TypeVar, Callable, List
 
 import numpy as np
 from rclpy.node import Node
@@ -14,15 +13,22 @@ class Weights:
     slope: float = 1
 
 
-state_t = TypeVar('state_t')  # np.ndarray[[3], float]
-path_t = TypeVar('path_t')  # np.ndarray[[-1, 2], float]
-
-
 @dataclass
 class VelParams:
     m: float = 730.0
     mu: float = 0.6
     rolling_friction: float = 65.0
+
+
+@dataclass
+class CollisionParams:
+    circle_offset = 0
+    circle_radii = 1
+    growth_factor = 0
+
+
+state_t = TypeVar('state_t')  # np.ndarray[[3], float]
+path_t = TypeVar('path_t')  # np.ndarray[[-1, 2], float]
 
 
 @dataclass
@@ -32,7 +38,11 @@ class Env:
 
     weights: Weights = Weights()
     vel_params: VelParams = VelParams()
-    info = None
+    collision_params: CollisionParams = CollisionParams()
+    info: Callable[[str], None] = None
 
     state: state_t = np.zeros(4)  # [x, y, theta, v]
     path: path_t = None  # [ [x, y], ... ]
+
+    obstacles: np.ndarray = np.zeros((2, 0))
+    other_vehicle_states: List[state_t] = ()
